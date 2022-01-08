87843032a2479ed905e88f6f499c7a0525877c11
Alex Shipula <alexshipula@gmail.com>
2022-01-08 12:17:38 +0200

    new commit

--- src/Metamask/Contract.ts
+++ src/Metamask/Contract.ts
@@ -1,2 +1,2 @@
 export const RinkebyTestContract = "0x4908a8fd956fd5f027f95c0495f02e735ae4a9ee";
-export const MixerTestContract = "0xCe9D286ea76Fd22edFa8c884EA75f405e51d3858";
+//export const MixerTestContract = "0xCe9D286ea76Fd22edFa8c884EA75f405e51d3858";
